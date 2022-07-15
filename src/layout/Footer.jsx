d254ec2da6ae1edc429b09b0b9fdbb6d368e3390
yeana-dev <yeana.dev@gmail.com>
2022-07-15 17:39:57 -0500

    Created footer component

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -12,13 +12,33 @@ export default function Footer() {
             <li>Schedule</li>
             <li>Contact Us</li>
           </ul>
-          <p className="copyright">Copyright © Master D. Choi Taekwondo 2022. All rights reserved.</p>
+          <p className='copyright'>
+            Copyright © Master D. Choi Taekwondo 2022. All rights reserved.
+          </p>
         </section>
         <section className='footer-menu__right'>
           <h2>Master D. Choi Taekwondo</h2>
-          <p>123 Your Street #123 Houston, TX 12345</p>
-          <p>(123) 123-4567</p>
-          <p>info@masterdchoi.com</p>
+          <p>
+            <a
+              href='https://goo.gl/maps/UXEWZ2Ryi2AHWXwx9'
+              target='_blank'
+              rel='noreferrer'
+            >
+              18310 W Airport Blvd Suite 700, Richmond, TX 77407
+            </a>
+          </p>
+          <p>
+            <a href='tel:832-282-8633'>(832) 282-8633</a>
+          </p>
+          <p>
+            <a
+              href='mailto:info@masterdchoi.com'
+              target='_blank'
+              rel='noreferrer'
+              >
+              info@masterdchoi.com
+            </a>
+          </p>
           <h3>Hours</h3>
           <div>
             <p>
