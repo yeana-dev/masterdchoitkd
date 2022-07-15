832b575d81717a7d6ed766b3ad99ed75cb5e3e9c
yeana-dev <yeana.dev@gmail.com>
2022-07-15 17:39:35 -0500

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 import Layout from './layout/Layout';
 import Home from './screens/Home';
+import About from './screens/About';
+import Programs from './screens/Programs';
+import Schedule from './screens/Schedule';
+import ContactUs from './screens/ContactUs';
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <Home />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/programs" element={<Programs />} />
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+        </Routes>
       </Layout>
     </div>
   );
