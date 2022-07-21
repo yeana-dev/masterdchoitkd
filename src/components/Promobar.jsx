7751597a378506df5a138a8904fd23d976732848
yeana-dev <yeana.dev@gmail.com>
2022-07-20 20:28:29 -0500

    Added mobile size styling

--- src/components/Promobar.jsx
+++ src/components/Promobar.jsx
@@ -4,8 +4,15 @@ export default function Promobar() {
   return (
     <div className='promobar'>
       <p className='promobar__text'>
-        GRAND OPENING 🎉 18310 W Airport Blvd Suite 700, Richmond, TX 77407 👋
-        CLICK HERE TO CALL TODAY!
+        GRAND OPENING 🎉{' '}
+        <a
+          href='https://goo.gl/maps/UXEWZ2Ryi2AHWXwx9'
+          target='_blank'
+          rel='noreferrer'
+        >
+          18310 W Airport Blvd Suite 700, Richmond, TX 77407
+        </a>{' '}
+        📞 <a href='tel:832-282-8633'>CLICK HERE TO CALL TODAY!</a>
       </p>
     </div>
   );
